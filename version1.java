08ee4491c854968fdc11b1e7e70023a1720fe68d
My-Linh Ho <mylinhho@u.boisestate.edu>
2023-09-06 14:25:33 -0600

    from branch dev 1

--- version1.java
+++ version1.java
@@ -7,6 +7,7 @@ public class version1{
     public static void main(String[] agrs){
         System.out.println("this is on merge comflict branch test2");
         System.out.println("this is on merge comflict branch dev test3");
+        System.out.println("this is on merge comflict branch dev test4");
         version1 version = new version1();
         version.isSaying();
     }
