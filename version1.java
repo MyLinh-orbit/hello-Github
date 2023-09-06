87b70e56836dbb737cfff11a9148c011aaeed331
My-Linh Ho <mylinhho@u.boisestate.edu>
2023-09-06 13:38:56 -0600

    update from master

--- version1.java
+++ version1.java
@@ -3,7 +3,7 @@ public class version1{
             return true;
     }
     public static void main(String[] agrs){
-        System.out.println("this is on merge comflict branch");
+        System.out.println("this is merge from master");
         version1 version = new version1();
         version.isSaying();
     }
