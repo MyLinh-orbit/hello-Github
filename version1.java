a550c17da108a5450819838f1ec88d984ff337f2
My-Linh Ho <mylinhho@u.boisestate.edu>
2023-09-06 13:37:10 -0600

    update from branch dev

--- version1.java
+++ version1.java
@@ -1,9 +1,11 @@
 public class version1{
     private boolean isSaying(){
+        int n = 0;
+        n++;
             return true;
     }
     public static void main(String[] agrs){
-        System.out.println("this is on merge comflict branch");
+        System.out.println("this is on merge comflict branch test2");
         version1 version = new version1();
         version.isSaying();
     }
